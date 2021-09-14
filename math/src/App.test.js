0171013567280c9a48c0c17b70b8f440d70d1d83
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-14 18:18:13 +0100

    Fix linters

--- math/src/App.test.js
+++ math/src/App.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+
 import { render, screen } from '@testing-library/react';
-// import App from './App';
+import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
