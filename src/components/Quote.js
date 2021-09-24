17ec1436daa5d3ced113b54edba4a265a7d44fc6
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-25 00:09:43 +0100

    Adds the quotation page

---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Quote = () => (
+  <>
+    <section className="home1">
+      <div className="content1">
+        <p>
+          Mathematics is not about numbers, equations,
+          computations or algorithms. It is about understanding - William Paul Thurston
+        </p>
+      </div>
+    </section>
+  </>
+);
+
+export default Quote;
